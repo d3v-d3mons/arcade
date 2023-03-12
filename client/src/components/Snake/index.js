d51149ba4680b454eb5d530981777c64de162e25
nic <nrhoda14@gmail.com>
2023-03-12 11:41:59 -0500

    specified engine in client packages, added snake component going to continue work on it today, created snake page and linked it to render conditionally on home[sgr page

---
+++ client/src/components/Snake/index.js
@@ -0,0 +1,19 @@
+import React, { useState, useEffect } from "react";
+// import { useDispatch, useSelector } from 'react-redux';
+
+ export default function Board () {
+    // const dispatch = useDispatch();
+    // const score = useSelector(state => state.score);
+    // const gameOver = useSelector(state => state.gameOver);
+
+    // const [snake, setSnake] = useState([
+    //     { x: 10, y: 10 },
+    //     { x: 10, y: 9 },
+    //     { x: 10, y: 8 },
+    // ]);
+
+    return (
+        <h1>Snek</h1>
+    )
+};
+
