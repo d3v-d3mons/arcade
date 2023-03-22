d257b4bda25daf132e94ce75d834f2a66e61f2f2
Moses C <113498167+MoZCa@users.noreply.github.com>
2023-03-21 19:14:09 -0500

    testing tee time

--- client/src/components/NewPlayer/index.js
+++ client/src/components/NewPlayer/index.js
@@ -1,50 +1,52 @@
 import React, { useState, useEffect, useContext } from "react";
-import { Button, Input } from "@chakra-ui/react";
+import { Button, Input, Center } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import { ADD_PLAYER } from "../../utils/mutations";
 import { useMutation } from "@apollo/client";
 
 export default function NewPlayer() {
   const [addPlayer] = useMutation(ADD_PLAYER);
+  const addNewPlayer = async () => {
+    if (player1) {
+      await addPlayer({
+        variables: {
+          partyName: partyName,
+          name: player1,
+          score: 0,
+        },
+      });
+    }
 
-  const addNewPlayer1 = async () => {
-    const player = await addPlayer({
-      variables: {
-        partyName: partyName,
-        name: player1,
-        score: 0,
-      },
-    });
-  };
+    if (player2) {
+      await addPlayer({
+        variables: {
+          partyName: partyName,
+          name: player2,
+          score: 0,
+        },
+      });
+    }
 
-  const addNewPlayer2 = async () => {
-    const player = await addPlayer({
-      variables: {
-        partyName: partyName,
-        name: player2,
-        score: 0,
-      },
-    });
-  };
+    if (player3) {
+      await addPlayer({
+        variables: {
+          partyName: partyName,
+          name: player3,
+          score: 0,
+        },
+      });
+    }
 
-  const addNewPlayer3 = async () => {
-    const player = await addPlayer({
-      variables: {
-        partyName: partyName,
-        name: player3,
-        score: 0,
-      },
-    });
-  };
-
-  const addNewPlayer4 = async () => {
-    const player = await addPlayer({
-      variables: {
-        partyName: partyName,
-        name: player4,
-        score: 0,
-      },
-    });
+    if (player4) {
+      await addPlayer({
+        variables: {
+          partyName: partyName,
+          name: player4,
+          score: 0,
+        },
+      });
+    }
+    setFormDone(true);
   };
 
   const [player1, setPlayer1] = useState("");
@@ -52,65 +54,69 @@ export default function NewPlayer() {
   const [player3, setPlayer3] = useState("");
   const [player4, setPlayer4] = useState("");
   const [partyName, setPartyName] = useState("");
+  const [formDone, setFormDone] = useState(false);
 
-  return (
-    <>
-      <div className="addPlayer">
-        <Input
-          className="gamePartyName"
-          type="text"
-          value={partyName}
-          placeholder="enter party name here"
-          onChange={(e) => setPartyName(e.target.value)}
-        />
-        <Input
-          className="playerName"
-          type="text"
-          value={player1}
-          placeholder="enter player 1 name here"
-          onChange={(e) => setPlayer1(e.target.value)}
-        />
-        <button className="playerBtn" onClick={addNewPlayer1}>
-          Add Player
-        </button>
-        <Input
-          className="playerName"
-          type="text"
-          value={player2}
-          placeholder="enter player 2 name"
-          onChange={(e) => setPlayer2(e.target.value)}
-        />
-        <button className="playerBtn" onClick={addNewPlayer2}>
-          Add Player
-        </button>
-        <Input
-          className="playerName"
-          type="text"
-          value={player3}
-          placeholder="enter player 3 name"
-          onChange={(e) => setPlayer3(e.target.value)}
-        />
-        <button className="playerBtn" onClick={addNewPlayer3}>
-          Add Player
-        </button>
-        <Input
-          className="playerName"
-          type="text"
-          value={player4}
-          placeholder="enter player 4 name"
-          onChange={(e) => setPlayer4(e.target.value)}
-        />
-        <button className="playerBtn" onClick={addNewPlayer4}>
-          Add Player
-        </button>
-        <Button
-          type="button"
-          colorScheme="green"
-          onClick={window.location.replace("/scorecard")}
-        >
-          Go to scorecard
+  if (formDone === false) {
+    return (
+      <>
+        <Center>
+          <div className="addPlayer">
+            <h1 className="partyNameTitle">Party Name:</h1>
+            <Input
+              className="gamePartyName"
+              type="text"
+              value={partyName}
+              placeholder="enter party name here"
+              onChange={(e) => setPartyName(e.target.value)}
+            />
+            <h1 className="playerTitle">Player 1:</h1>
+            <Input
+              className="playerName"
+              type="text"
+              value={player1}
+              placeholder="enter player 1 name here"
+              onChange={(e) => setPlayer1(e.target.value)}
+            />
+            <h1 className="playerTitle">Player 2:</h1>
+            <Input
+              className="playerName"
+              type="text"
+              value={player2}
+              placeholder="enter player 2 name"
+              onChange={(e) => setPlayer2(e.target.value)}
+            />
+            <h1 className="playerTitle">Player 3:</h1>
+            <Input
+              className="playerName"
+              type="text"
+              value={player3}
+              placeholder="enter player 3 name"
+              onChange={(e) => setPlayer3(e.target.value)}
+            />
+            <h1 className="playerTitle">Player 4:</h1>
+            <Input
+              className="playerName"
+              type="text"
+              value={player4}
+              placeholder="enter player 4 name"
+              onChange={(e) => setPlayer4(e.target.value)}
+            />
+            </div>
+            </Center>
+            <Center>
+            <Button className="playerBtn" onClick={addNewPlayer}>
+              Add Players
+            </Button>
+        </Center>
+      </>
+    );
+  } else {
+    return (
+      <Center>
+        <Button className="scorecardDirect">
+          <Link to="/scorecard"></Link>
         </Button>
-      </div>
-    </>
-  );
+      </Center>
+    );
+  }
 }
